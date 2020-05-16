058e6fea4d4722fae8c0463ccd56b6bff619c490
kerovinoddi <kerovineoddi@gmail.com>
2020-05-16 15:15:54 +0300

    added a README.md

---
+++ Js/script.js
@@ -0,0 +1,18 @@
+<button onclick="myFunction()">click</button>
+
+<script>
+function myFunction() {
+  var x = document.createElement("VIDEO");
+
+  if (x.canPlayType("video/mp4")) {
+    x.setAttribute("src","movie.mp4");
+  } else {
+    x.setAttribute("src","movie.ogg");
+  }
+
+  x.setAttribute("width", "320");
+  x.setAttribute("height", "240");
+  x.setAttribute("controls", "controls");
+  document.body.appendChild(x);
+}
+</script>
