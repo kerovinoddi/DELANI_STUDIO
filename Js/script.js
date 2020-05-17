95bbebb8fa947e6827411bea6823f50fa67747ce
user name <user email>
2020-05-17 22:24:14 +0300

    added a paragraph

--- Js/script.js
+++ Js/script.js
@@ -1,82 +1,64 @@
 
-// <button onclick="myFunction()">click</button>
 
-
-// function myFunction() {
-//   var x = document.createElement("VIDEO");
-
-//   if (x.canPlayType("video/mp4")) {
-
-//     x.setAttribute("src","movie.mp4");
-//   } else {
-//     x.setAttribute("src","movie.ogg");
-//   }
-
-//   x.setAttribute("width", "320");
-//   x.setAttribute("height", "240");
-//   x.setAttribute("controls", "controls");
-//   document.body.appendChild(x);
-// }
-
-$(document).ready(function(){
-  $(#img-1).click(function(){
+$(document).ready(function () {
+  $(#img - 1).click(function () {
     $(.design).toggle();
   })
 })
-$(document).ready(function(){
-  $(#img-2).click(function(){
+$(document).ready(function () {
+  $(#img - 2).click(function () {
     $(.dev).toggle();
   })
 })
-$(document).ready(function(){
-  $(#img-3).click(function(){
+$(document).ready(function () {
+  $(#img - 3).click(function () {
     $(.product).toggle();
   })
 })
 
-$("document").ready(function(){
-  $(".row .col-md-3 #one").mouseenter(function(){
-      $(".row .col-md-3 #first").show();
+$("document").ready(function () {
+  $(".row .col-md-3 #one").mouseenter(function () {
+    $(".row .col-md-3 #first").show();
   })
-  $(".row .col-md-3 #two").mouseleave(function(){
-      $(".row .col-md-3 #second").hide();
+  $(".row .col-md-3 #two").mouseleave(function () {
+    $(".row .col-md-3 #second").hide();
   })
-  $(".row .col-md-3 #three").mouseleave(function(){
+  $(".row .col-md-3 #three").mouseleave(function () {
     $(".row .col-md-3 #third").hide();
   })
-  $(".row .col-md-3 #four").mouseleave(function(){
+  $(".row .col-md-3 #four").mouseleave(function () {
     $(".row .col-md-3 #five").hide();
-   })
+  })
 });
 
 
-  $(document).ready(function()
+$(document).ready(function ()
   $(".row .col-md-3 #five").mouseleave(functio(){
-    $(".row .col-md-3 #fifth").hide();
-  })
-  $(".row .col-md-3 #six").mouseleave(function(){
-    $(".row .col-md-3 #sixth").hide();
-  })
-  $(".row .col-md-3 #seven").mouseleave(function(){
-    $(".row .col-md-3 #seventh").hide
-  })
-  $(".row .col-md-3 #eight").mouseleave(function(){
-    $(".row .col-md-3 #eighth").hide(;)
+  $(".row .col-md-3 #fifth").hide();
+})
+  $(".row .col-md-3 #six").mouseleave(function () {
+  $(".row .col-md-3 #sixth").hide();
+})
+  $(".row .col-md-3 #seven").mouseleave(function () {
+  $(".row .col-md-3 #seventh").hide
+})
+  $(".row .col-md-3 #eight").mouseleave(function () {
+  $(".row .col-md-3 #eighth").hide(;)
   })
 
 })
 
 $("#button").click(function () {
   event.preventDefault()
- var name= $("#name").val();
- var email= $("#email").val();
-  var message=$("#textarea").val();
-  if(name==="" || email==="" || message===""){
-      alert("Kindly key in your information in the output below!");
-     
+  var name = $("#name").val();
+  var email = $("#email").val();
+  var message = $("#textarea").val();
+  if (name === "" || email === "" || message === "") {
+    alert("This information is required we value it please fill in!");
+
   }
-  else{
-      alert("Good work we have received your information and we are on progress!");
+  else {
+    alert("Great  we are pleased with you and thanks for contacting us we are looking forward to serve you!");
 
   }
 
